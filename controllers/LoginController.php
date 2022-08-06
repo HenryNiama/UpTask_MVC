90931938019c5c305420c10fef67ef29b77676c7
Henry <hsniama@utpl.edu.ec>
2022-08-05 23:16:35 -0500

    Validando que el token sea valido

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -128,15 +128,33 @@ class LoginController{
         ]);
     }
 
-    public static function reestablecer(Router $router){       
+    public static function reestablecer(Router $router){     
+        
+        $token = s($_GET['token']);
+        $mostrar = true;
+
+        if(!$token) header('Location: /');
+
+        // Identificar el usuario con este token
+        $usuario = Usuario::where('token', $token);
+
+        if(empty($usuario)){
+            Usuario::setAlerta('error', 'Token No Valido');
+            $mostrar = false;
+        }
+
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             # code...
         }
 
+        $alertas = Usuario::getAlertas();
+
         // Render a la vista
         $router->render('auth/reestablecer', [
-            'titulo' => 'Reestablecer Password...'
+            'titulo' => 'Reestablecer Password...',
+            'alertas' => $alertas,
+            'mostrar' => $mostrar
         ]);
     }
 
--- views/auth/reestablecer.php
+++ views/auth/reestablecer.php
@@ -5,6 +5,10 @@
     <div class="contenedor-sm">
         <p class="descripcion-pagina">Coloca tu nuevo Password</p>
         
+        <?php include_once __DIR__ . '/../templates/alertas.php'; ?>
+
+        <?php if($mostrar) {?>
+
         <form class="formulario" action="/reestablecer" method="POST">
 
             <div class="campo">
@@ -30,6 +34,8 @@
             <input type="submit" class="boton" value="Guardar Password">
 
         </form>
+
+        <?php }?>
 
         <div class="acciones">
             <a href="/crear">Aún no tienes una cuenta? Obtén una!</a>
