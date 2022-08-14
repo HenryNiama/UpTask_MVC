9ed37acbb367e61772d695511cd141b049e399df
Henry <hsniama@utpl.edu.ec>
2022-08-14 15:47:52 -0500

    Agregando boton para cerrar el modal

--- src/js/tareas.js
+++ src/js/tareas.js
@@ -32,6 +32,21 @@
             formulario.classList.add('animar'); 
         }, 500);
 
+        modal.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            if (e.target.classList.contains('cerrar-modal')) {
+
+                const formulario = document.querySelector('.formulario');
+                formulario.classList.add('cerrar'); 
+
+                setTimeout(() => {
+                    modal.remove();
+                }, 500);
+            }
+            
+        });
+
         document.querySelector('body').appendChild(modal);
     }
 })();
