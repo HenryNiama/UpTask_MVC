6628608ff62b9efcf020c766d106254cb0026ef5
Henry <hsniama@utpl.edu.ec>
2022-08-25 18:55:07 -0500

    Mostrando/Ocultando el menu para celular

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,20 @@
 const mobileMenuBtn = document.querySelector('#mobile-menu');
 const sidebar = document.querySelector('.sidebar');
+const cerrarMenuBtn = document.querySelector('#cerrar-menu');
 
 if (mobileMenuBtn) {
     mobileMenuBtn.addEventListener('click', function () {
-        sidebar.classList.toggle('mostrar');
+        sidebar.classList.add('mostrar');
+    });
+}
+
+if (cerrarMenuBtn) {
+    cerrarMenuBtn.addEventListener('click', function () {
+        sidebar.classList.add('ocultar');
+
+        setTimeout(() => {
+            sidebar.classList.remove('mostrar');
+            sidebar.classList.remove('ocultar');
+        }, 1000);
     });
 }
--- views/templates/sidebar.php
+++ views/templates/sidebar.php
@@ -1,5 +1,13 @@
 <aside class="sidebar">
-    <h2>UpTask</h2>
+
+    <div class="contenedor-sidebar">
+        <h2>UpTask</h2>
+
+        <div class="cerrar-menu">
+            <img src="build/img/cerrar.svg" alt="Imagen cerrar menu" id="cerrar-menu">
+        </div>
+    </div>
+
 
     <nav class="sidebar-nav">
         <a class="<?php echo($titulo === 'Proyectos') ? 'activo' : ''; ?>" href="/dashboard">Proyectos</a>
